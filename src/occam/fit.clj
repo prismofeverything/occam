148b6793e36f8931eaac75f78cae6b22ba4499bc
ryan spangler <ryan.spangler@gmail.com>
2012-11-09 10:28:03 -0800

    making patterns from fit tables

--- src/occam/fit.clj
+++ src/occam/fit.clj
@@ -7,5 +7,7 @@
   (let [raw (slurp filename)
         lines (string/split raw #"\n")
         portions (map #(string/split % #"\|") lines)
-        entries (map #(map (fn [p] (string/split (string/trim p) #"\t")) %) portions)]
+        entries (map #(map (fn [p] (string/split (string/trim p) #"\t")) %) portions)
+        [variables observed calculated] (first entries)
+        maps (rest entries)]
     entries))
